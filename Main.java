e93777f902545351b0f24e44d1e42ba11bf59f01
youssef <youssefmahni11@gmail.com>
2022-10-29 23:21:32 +0100

    the main file is needed to call the other files

---
+++ Main.java
@@ -0,0 +1,9 @@
+package ClientSide;
+
+public class Main {
+    public static void main(String[] args) {
+//        WelcomePage page1 = new WelcomePage();
+//just a test
+        MenuPage page2 = new MenuPage();
+    }
+}
